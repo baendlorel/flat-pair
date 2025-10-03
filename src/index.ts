d4f3be8399705ff1daa5efa5d9ece0e08ab74f46
aldia_mechdream <futami16237@gmail.com>
2025-10-03 18:57:17 +0800

    feat: operators单独放置

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './flat-pair.js';
 export * from './flat-pair-operator.js';
+export * from './operators.js';
---
+++ src/operators.ts
@@ -0,0 +1,47 @@
+export function add<K, V>(items: any[], key: K, value: V): void {
+  for (let i = 0; i < items.length; i += 2) {
+    if (items[i] === key) {
+      return;
+    }
+  }
+  items.push(key, value);
+}
+
+export function size(items: any[]): number {
+  if (items.length % 2 !== 0) {
+    throw new Error('Invalid items length, must be even number');
+  }
+  return items.length / 2;
+}
+
+export function find<K, V>(items: any[], key: K): V | undefined {
+  for (let i = 0; i < items.length; i += 2) {
+    if (items[i] === key) {
+      return items[i + 1];
+    }
+  }
+  return undefined;
+}
+
+export function findByValue<K, V>(items: any[], value: V): K | undefined {
+  for (let i = 1; i < items.length; i += 2) {
+    if (items[i] === value) {
+      return items[i - 1];
+    }
+  }
+  return undefined;
+}
+
+export function remove<K>(items: any[], key: K): boolean {
+  for (let i = 0; i < items.length; i += 2) {
+    if (items[i] === key) {
+      items.splice(i, 2);
+      return true;
+    }
+  }
+  return false;
+}
+
+export function clear(items: any[]): void {
+  items.length = 0;
+}
